bf6cfe0fb0922c417ebd0570a44acdeb99a6f664
Jason Shive <jwshive@gmail.com>
2021-04-03 12:29:52 -0400

    Removing the discord and email address. Will mark them optional in another table

--- srv/models/user-model.js
+++ srv/models/user-model.js
@@ -1,12 +1,9 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-require("mongoose-type-email");
 
 const userSchema = new Schema({
   battletag: String,
   bnetID: Number,
-  emailAddress: { type: mongoose.SchemaTypes.Email, required: false },
-  discordName: { type: String, required: false },
   accessToken: String,
 });
 
